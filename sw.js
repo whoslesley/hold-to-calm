a2c9dec4e59c5b98fd5f02af3620767b5e980931
Oran_ja <154502682+whoslesley@users.noreply.github.com>
2026-06-12 06:37:54 +0800

    Add files via upload
    诊断版！！

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'anzhu-v1';
+const C = 'anzhu-v3';
 const FILES = ['.', 'index.html', 'manifest.json', 'icon.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(C).then(c => c.addAll(FILES))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k))))));
