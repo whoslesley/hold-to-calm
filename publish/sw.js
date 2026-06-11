ab895098713ac92570810fa5556e9719d0d09bb4
Oran_ja <154502682+whoslesley@users.noreply.github.com>
2026-06-12 06:01:41 +0800

    Add files via upload

---
+++ publish/sw.js
@@ -0,0 +1,5 @@
+const C = 'anzhu-v1';
+const FILES = ['.', 'index.html', 'manifest.json', 'icon.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(C).then(c => c.addAll(FILES))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k))))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
